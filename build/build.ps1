5da81d537f2184df19e588aff1baf35060b93a57
Emmanuel Benitez <eben@aprico-consult.com>
2020-01-18 23:15:00 +0100

    Include code coverage and review versioning

--- build/build.ps1
+++ build/build.ps1
@@ -3,33 +3,23 @@
 param(
     [Parameter(Mandatory = $false)]
     [int]
-    $Major = 1,
-
-    [Parameter(Mandatory = $false)]
-    [int]
-    $Minor = 0,
-
-    [Parameter(Mandatory = $false)]
-    [int]
     $Build = ('{0:yy}{1:000}' -f [datetime]::Today, [datetime]::Today.DayOfYear),
 
     [Parameter(Mandatory = $false)]
     [int]
-    $Revision = (([datetime]::Now - [datetime]::Today).TotalSeconds / 1.4)
+    $Revision = (([datetime]::Now - [datetime]::Today).TotalSeconds / 1.4),
+
+    [Parameter(Mandatory = $false)]
+    [bool]
+    $PreRelease = $true,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $PrivateKeyPath
 )
 
-Clear-Host
-
-# construct a Version object to ensure arguments are valid
-$version = New-Object -TypeName System.Version -ArgumentList $Major, $Minor, $Build, $Revision
+#Clear-Host
 
 # build and package solution
 #https://docs.microsoft.com/en-us/nuget/create-packages/symbol-packages-snupkg
-dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-dotnet build -p:DelaySign=false`;Prerelease=true`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true
-
-# generate build.local.ps1 script file that allows to redo a build and package locally wihtout altering the build version number
-$path = Split-Path $script:MyInvocation.MyCommand.Path
-@"
-& $(Join-Path $path build.ps1) -Major $($version.Major) -Minor $($version.Minor) -Build $($version.Build) -Revision $($version.Revision)
-"@ > $(Join-Path $path build.local.ps1)
+dotnet build -p:DelaySign=false`;PreRelease=true`;Configuration=Release`;Build=$($Build)`;Revision=$($Revision)`;AssemblyOriginatorKeyFile=$($PrivateKeyPath)`;GeneratePackageOnBuild=true
