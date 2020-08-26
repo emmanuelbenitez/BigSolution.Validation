b990873ece4ed5f7a5f988237ba73422a513cb01
Emmanuel Benitez <eben@aprico-consult.com>
2020-08-26 11:18:45 +0200

    Add IsInterface constraint

---
+++ src/BigSolution.Validation.Tests/Constraints/TypeConstraintsFixture.cs
@@ -0,0 +1,53 @@
+﻿#region Copyright & License
+
+// Copyright © 2020 - 2020 Emmanuel Benitez
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using Xunit;
+
+namespace BigSolution
+{
+    public class TypeConstraintsFixture
+    {
+        [Fact]
+        [SuppressMessage("ReSharper", "NotResolvedInText", Justification = "Testing purpose")]
+        public void IsInterfaceFailed()
+        {
+            Action act = () => Requires.Argument(typeof(object), "param")
+                .IsInterface()
+                .Check();
+
+            act.Should().ThrowExactly<ArgumentException>()
+                .WithMessage($"The type '*' is not an interface. (Parameter 'param')")
+                .Which.ParamName.Should().Be("param");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(typeof(IArgumentValidation))]
+        public void IsInterfaceSucceeds(Type type)
+        {
+            Action act = () => Requires.Argument(type, nameof(type))
+                .IsInterface()
+                .Check();
+
+            act.Should().NotThrow();
+        }
+    }
+}
---
+++ src/BigSolution.Validation/Resources/Resources.TypeConstraints.cs
@@ -0,0 +1,37 @@
+﻿#region Copyright & License
+
+// Copyright © 2020 - 2020 Emmanuel Benitez
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+
+namespace BigSolution
+{
+    internal static partial class Resources
+    {
+        #region Nested Type: TypeConstraints
+
+        public static class TypeConstraints
+        {
+            public static string IsInterfaceErrorMessage(Type type)
+            {
+                return GetFormattedString($"{nameof(TypeConstraints)}_{nameof(IsInterfaceErrorMessage)}", null, type?.AssemblyQualifiedName);
+            }
+        }
+
+        #endregion
+    }
+}
